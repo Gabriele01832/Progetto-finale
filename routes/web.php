e792c1f107c5dc335c5f93764eaa86afc4d268b8
Gabriele Vargiu <gabriele.v_@virgilio.it>
2025-03-21 16:51:16 +0100

    Aggiornato file routes/web.php con gestione modifica ed eliminazione articoli

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', [PublicController::class, 'home'])->name('home');
 
 // Articoli
@@ -13,6 +19,11 @@ Route::get('/articles', [ArticleController::class, 'index'])->name('articles.ind
 Route::get('/articles/create', [ArticleController::class, 'create'])->middleware(['auth'])->name('articles.create');
 Route::post('/articles/store', [ArticleController::class, 'store'])->middleware(['auth'])->name('articles.store');
 Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+
+// Modifica ed eliminazione articoli (solo autore)
+Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->middleware(['auth'])->name('articles.edit');
+Route::put('/articles/{article}', [ArticleController::class, 'update'])->middleware(['auth'])->name('articles.update');
+Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])->middleware(['auth'])->name('articles.destroy');
 
 // Autore
 Route::get('/author/{user}', [UserController::class, 'show'])->name('author.show');
